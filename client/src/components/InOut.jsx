11d74acd9c2688d9a68a9b7964914ad62000bd22
JOHNVENEY <Johnveney@gmail.com>
2022-12-06 17:00:51 -0500

    more work on layout.

--- client/src/components/InOut.jsx
+++ client/src/components/InOut.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import { slideScreen } from "../Tools";
-import teamchat from "../graphics/Teams-16x16.png";
+import teamchat from "../graphics/Teams-24x24.png";
+import newemail from "../graphics/email.png";
+import newphone from "../graphics/phone.png";
 
 function InOut() {
   const [loading, setLoading] = useState(false);
@@ -23,7 +25,6 @@ function InOut() {
   return (
     <div ref={outerWrapper} className="main-panel p-bottom-4 bg-neutral-1 ">
       <header className="p-1 p-top-2 flex-align-items-center bg-white">
-        <h1>MIRK INC</h1>
         <h2>
           <button
             type="button"
@@ -52,7 +53,7 @@ function InOut() {
               <tr>
                 <th>Employee</th>
                 <th class="td_status">Status</th>
-                <th>&nbsp;</th>
+                <th>Contact</th>
                 <th>Notes</th>
               </tr>
             </thead>
@@ -64,27 +65,88 @@ function InOut() {
                 <td class="td_first">Baker, Tina</td>
                 <td class="td_status">Vacation</td>
                 <td class="td_contact">
-                <a class="contact_anchor"
+                  <a
+                    class="contact_anchor"
                     target="tbakerchat"
+                    title="Chat via Teams"
                     href="https://teams.microsoft.com/l/chat/0/0?users=bakert@mirkinc.us"
                   >
-                    <img src={teamchat} className="radish-icon-medium" alt="radish" />
+                    <img
+                      src={teamchat}
+                      className="radish-icon-24"
+                      alt="start team chat"
+                    />
                   </a>
-                  <a class="contact_anchor" href="tel:330-641-2303">330-641-2303</a>
-                  <a class="contact_anchor" href="mailto:bakert@mirkinc.us">mail</a>
+                  <a
+                    class="contact_anchor"
+                    title="Moble call"
+                    href="tel:330-641-2303"
+                  >
+                    <img
+                      src={newphone}
+                      className="radish-icon-24"
+                      alt="start mobile call"
+                    />
+                  </a>
+                  <a
+                    class="contact_anchor"
+                    title="Send email"
+                    href="mailto:bakert@mirkinc.us"
+                  >
+                    <img
+                      src={newemail}
+                      className="radish-icon-24"
+                      alt="start new email"
+                    />
+                  </a>
                 </td>
-                <td>
-                  Out Until 12/5
-                </td>
+                <td>Out Until 12/5</td>
               </tr>
               <tr class="tr_reverse">
                 <td>Thut, Mike</td>
                 <td class="td_status">In Office</td>
+                <td class="td_contact">
+                  <a
+                    class="contact_anchor"
+                    target="tbakerchat"
+                    title="Chat via Teams"
+                    href="https://teams.microsoft.com/l/chat/0/0?users=bakert@mirkinc.us"
+                  >
+                    <img
+                      src={teamchat}
+                      className="radish-icon-24"
+                      alt="start team chat"
+                    />
+                  </a>
+                  <a
+                    class="contact_anchor"
+                    title="Moble call"
+                    href="tel:330-641-2303"
+                  >
+                    <img
+                      src={newphone}
+                      className="radish-icon-24"
+                      alt="start mobile call"
+                    />
+                  </a>
+                  <a
+                    class="contact_anchor"
+                    title="Send email"
+                    href="mailto:bakert@mirkinc.us"
+                  >
+                    <img
+                      src={newemail}
+                      className="radish-icon-24"
+                      alt="start new email"
+                    />
+                  </a>
+                </td>
                 <td></td>
               </tr>
               <tr>
                 <td>Thut, Shirley</td>
                 <td class="td_status">Vacation</td>
+                <td></td>
                 <td>
                   Out Until 12/5
                   <a
@@ -98,6 +160,7 @@ function InOut() {
               <tr class="tr_reverse">
                 <td>Veney, John</td>
                 <td class="td_status">Remote</td>
+                <td></td>
                 <td>
                   At home until 9:00 In about 10:00 Cell:
                   <a href="tel:330-641-2303">330-641-2303</a>
@@ -118,25 +181,30 @@ function InOut() {
                 <td>I, Brian</td>
                 <td class="td_status">In Office</td>
                 <td></td>
+                <td></td>
               </tr>
               <tr class="tr_reverse">
                 <td>Friday, Chris</td>
                 <td class="td_status">Customer Delivery</td>
+                <td></td>
                 <td>Delivering Truck to xyz Company</td>
               </tr>
               <tr>
                 <td>S, Jamie</td>
                 <td class="td_status">In Office</td>
                 <td></td>
+                <td></td>
               </tr>
               <tr class="tr_reverse">
                 <td>Thut, Brock</td>
                 <td class="td_status">In Office</td>
                 <td></td>
+                <td></td>
               </tr>
               <tr>
                 <td>Z, Rick</td>
                 <td class="td_status">In Office</td>
+                <td></td>
                 <td></td>
               </tr>
               <tr class="location">
@@ -146,29 +214,38 @@ function InOut() {
                 <td>Cox, Kat</td>
                 <td class="td_status">In Office</td>
                 <td></td>
+                <td></td>
               </tr>
               <tr class="tr_reverse">
                 <td>Darby, Beth</td>
                 <td class="td_status">In Office</td>
+                <td></td>
                 <td></td>
               </tr>
               <tr>
                 <td>Haas, Bill</td>
                 <td class="td_status">In Office</td>
                 <td></td>
+                <td></td>
               </tr>
               <tr class="location">
                 <td colspan={colspan}>WAUKEGAN ILLINOIS</td>
               </tr>
               <tr>
+                <td>A, Daniel</td>
+                <td class="td_status">In Office</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
                 <td>H, James</td>
                 <td class="td_status">In Office</td>
                 <td></td>
+                <td></td>
               </tr>
+  
             </tbody>
-            <tr>
-              <td>hi</td>
-            </tr>
+          
           </table>
         </div>
       )}
